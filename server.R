cd997badabc71d809ffe88ca30b306f4228d74a9
poloarol <adjon081@uottawa.ca>
2023-01-10 16:23:05 -0500

    Fixed some UI problems. Added more analysis.

--- server.R
+++ server.R
@@ -6,26 +6,25 @@ if (!require('pacman')) install.packages("pacman")
 pacman::p_load(pacman, Seurat, tidyverse, shiny)
 
 
-load_data <- function(project, cells = 3, features){
+load_data <- function(project_name, num_cells = 3, num_features = 200){
     # Load the PBMC dataset
     reads <- Read10X(data.dir = "data/pbmc3k/filtered_gene_bc_matrices/hg19/")
     # Initialize the Seurat object with the raw (non-normalized data).
-    cts <- reads$`Gene Expression`
-    data <- CreateSeuratObject(
-            cts, 
-            project = project, 
-            min.cells = as.numeric(cells), 
-            min.features = as.numeric(features)
-            )
+    datum <- CreateSeuratObject(
+            counts = reads,
+            project = project_name,
+            min.cells = as.numeric(num_cells),
+            min.features = as.numeric(num_features))
     
-    return(reactiveVal(data))
+    datum[["percent.mt"]] <- PercentageFeatureSet(datum, pattern = "^MT-")
+    
+    return(datum)
 }
 
 
 metricsplot <- function(data){
   
   # seurat_object <- load_data(project, cells, features)
-  data[["percent.mt"]] <- PercentageFeatureSet(data, pattern = "^MT-")
   plt <- VlnPlot(data, 
                  features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), 
                  ncol = 3)
@@ -34,8 +33,6 @@ metricsplot <- function(data){
 }
 
 featureplot <- function(data){
-  
-  data[["percent.mt"]] <- PercentageFeatureSet(data, pattern = "^MT-")
   plot1 <- FeatureScatter(data, feature1 = "nCount_RNA", feature2 = "percent.mt")
   plot2 <- FeatureScatter(data, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
   
@@ -43,14 +40,104 @@ featureplot <- function(data){
   
 }
 
+subset_dataset <- function(data, min_rna = 200, max_rna = 2500, mito = 5){
+  results <- subset(data, 
+                    subset = nCount_RNA > min_rna & nFeature_RNA < max_rna &
+                      percent.mt < 5)
+  return(results)
+}
+
+normalize_dt <- function(data, strategy = "LogNormalize"){
+  
+  if(strategy == "scTransform"){
+    results <- SCTransform(data)
+  }else{
+    results <-  NormalizeData(data,  normalization.method = strategy)
+  }
+  
+  return(results)
+}
+
+variable_features <- function(data, strategy = "vst", features = 2000){
+  results <- FindVariableFeatures(data, 
+                                  selection.method = strategy, 
+                                  nfeatures = features)
+  return(results)
+}
+
+variable_features_plot <- function(data){
+  top10 <- head(VariableFeatures(data), 10)
+  plt1 <- VariableFeaturePlot(data)
+  plt2 <- LabelPoints(plot = plt1, points = top10, repel = TRUE)
+  plt <- plt1 + plt2
+  
+  return(plt)
+}
 
 server <- function(input, output, session) {
   
-  seurat_obj <- reactive({
-    load_data(input$proj_name, input$min.cells, input$min.feats)
+  seurat_obj <- eventReactive(
+    eventExpr = 
+      {
+      input$proj_name
+      input$min.cells
+      input$min.feats
+      }, {
+        load_data(
+          project_name = input$proj_name,
+          num_cells = input$min.cells,
+          num_features = input$min.feats)
+  })
+  
+  output$metrics <- renderPlot(metricsplot(seurat_obj()))
+  output$features <- renderPlot(featureplot(seurat_obj()))
+
+  subset_seurat <- eventReactive(
+    eventExpr =
+      {
+        input$min_cells
+        input$max_cells
+        input$mt
+        input$dblt
+      }, {
+        subset_dataset(
+          data = seurat_obj(),
+          min_rna = as.numeric(input$min_cells),
+          max_rna = as.numeric(input$max_cells),
+          mito = as.numeric(input$mt))
+      }
+  )
+  
+  nm_seurat <- eventReactive(
+    eventExpr = {
+      input$normalization
+    },{
+      normalize_dt(subset_seurat(), input$normalization)
     })
   
-  output$metrics <- renderPlot(metricsplot(seurat_obj))
-  output$features <- renderPlot(featureplot(seurat_obj))
+  hv_seurat <- eventReactive(
+    eventExpr = {
+      input$ftselection
+    }, {
+      variable_features(nm_seurat(), input$ftselection)
+    }
+  )
+
+  plt <- reactive({
+    variable_features_plot(hv_seurat())
+  })
   
+  scaled_seurat <- reactive({
+    all.genes <- rownames(hv_seurat())
+    ScaleData(hv_seurat(), all.genes)
+  })
+    
+    
+  output$topvariable <- renderPlot(plt())
 }
+
+
+
+
+
+
--- ui.R
+++ ui.R
@@ -9,32 +9,81 @@ ui <- fluidPage(
   navbarPage(
     "single-cell transcriptomics",
     tabPanel("Single-Cell Transcriptomics",
-             mainPanel(
+             mainPanel(width=12,
                
-               h2("Quality Control and Cell Selection"),
+               h2("1. Quality Control and Cell Selection"),
                
-               fluidRow(
-                 column(4, 
+               fluidRow(align="center",
+                 column(4,
+                        align = "center",
                         h4("Project Name"),
                         textInput("proj_name", NULL, value = "pbmc3k", width = NULL, placeholder = NULL)
                  ),
                  column(4,
+                        align = "center",
                         h4("Min. Number of Cells"),
-                        textInput("min.cells", NULL, value = 3, width = NULL, placeholder = NULL)
+                        textInput("min.cells", NULL, value = 10, width = NULL, placeholder = NULL)
                  ),
                  column(4,
+                        align = "center",
                         h4("Min. Number of features"),
                         textInput("min.feats", NULL, value = 200, width = NULL, placeholder = NULL)
                  )
                ),
                
                fluidRow(
-                 column(6, "QC Metrics",plotOutput("metrics")),
-                 column(6, "Feature-Feature Relationships", plotOutput("features"))
+                 column(6, align = "center", "QC Metrics",plotOutput("metrics")),
+                 column(6, align = "center", "Feature-Feature Relationships", plotOutput("features"))
                ),
                
-               width = 12
-               
+               h2("2. Filtering and Doublet removal"),
+              
+              fluidRow(
+                column(3,
+                       align = "center",
+                       h4("Min. Number of Genes Expressed"),
+                       textInput("min_cells", NULL, value = 200, width = NULL, placeholder = NULL)
+                ),
+                column(3,
+                       align = "center",
+                       h4("Max. number of Genes Expressed"),
+                       textInput("max_cells", NULL, value = 2500, width = NULL, placeholder = NULL)
+                ),
+                column(3,
+                       align = "center",
+                       h4("Percentage Mitochondrial Genes"),
+                       textInput("mt", NULL, value = 5, width = NULL, placeholder = NULL)
+                ),
+                column(3,
+                       h4("Dealing with Doublets"),
+                       checkboxInput("dblt", "Doublet Removal"))
+              ),
+              
+              h2("3. Identification of highly variable genes"),
+              
+              fluidRow(
+                column(4,
+                       h4("Parameters"),
+                       div(class="option-group",
+                           radioButtons("normalization",
+                                        "Normalization Strategy",
+                                        choices = c("LogNormalize", "CLR", "scTransform"),
+                                        inline = TRUE),
+                           radioButtons("ftselection",
+                                        "Feature Selection",
+                                        choices = c("vst", "mvp", "disp"),
+                                        inline = TRUE
+                                        ),
+                           # checkboxGroupInput("scaling-data",
+                           #              "Scaling Data Options",
+                           #              choices = c("Speed Up", "Remove unwanted sources of variation"))
+                           )),
+                       # actionButton('process-data-scaling', "Process Data")),
+                column(8,
+                       align = "center",
+                       h4("Top 10 most variable genes"),
+                       plotOutput("topvariable"))
+              )
                
                # ("")
                
